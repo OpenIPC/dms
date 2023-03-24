d3a03b3ccfe606a49d64fbc72ec4d9c4f6f16e78
Dmitry Ermakov <de@dimer.org.ua>
2023-03-24 14:46:18 +0300

    fixes

--- dms.py
+++ dms.py
@@ -20,8 +20,11 @@ from dvrip import DVRIPCam
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import uic, QtCore
+from PyQt5.QtCore import Qt, QSettings
 
 (Ui_MainWindow, QMainWindow) = uic.loadUiType('dms.ui')
+
+cam = DVRIPCam('')
 
 if os.path.exists('debug'):
     debug = True
@@ -31,16 +34,14 @@ else:
 if debug:
     debugLevel = logging.DEBUG
     devices = ({'00:00:00:00:00:64': {'ChannelNum': 9, 'DeviceType': 4, 'GateWay': '0xFE01A8C0', 'HostIP': '0x0A01A8C0', 'HostName': 'NBD80N32RA-KL', 'HttpPort': 80, 'MAC': '00:00:00:00:00:64', 'MaxBps': 0, 'MonMode': 'TCP', 'NetConnectState': 0, 'OtherFunction': 'D=2023-03-21 13:21:32 V=78775aada187e84', 'SN': '0000000000000081', 'SSLPort': 8443, 'Submask': '0x00FFFFFF', 'TCPMaxConn': 10, 'TCPPort': 34567, 'TransferPlan': 'Quality', 'UDPPort': 34568, 'UseHSDownLoad': False, 'Brand': 'xm'}, '00:00:00:00:00:17': {'ChannelNum': 1, 'DeviceType': 1, 'GateWay': '0x0101A8C0', 'HostIP': '0x0801A8C0', 'HostName': 'LocalHost', 'HttpPort': 80, 'MAC': '00:00:00:00:00:17', 'MaxBps': 0, 'MonMode': 'TCP', 'NetConnectState': 1, 'OtherFunction': 'D=2023-03-21 13:21:34 V=1482fd4408e15a7', 'SN': '0000000000000006', 'SSLPort': 8443, 'Submask': '0x00FFFFFF', 'TCPMaxConn': 10, 'TCPPort': 34567, 'TransferPlan': 'Quality', 'UDPPort': 34568, 'UseHSDownLoad': False, 'Brand': 'xm'}, '00:00:00:00:00:1e': {'BuildDate': '2020-07-04 09:25:14', 'ChannelNum': 1, 'GateWay': '0x0101A8C0', 'HostIP': '0x0701A8C0', 'HostName': 'IVG-85HF20PYA-S', 'HttpPort': 80, 'MAC': '00:00:00:00:00:1e', 'MonMode': 'TCP', 'NetConnectState': 1, 'OtherFunction': 'D=2023-03-21 13:21:34 V=d0384e6d8a46c2f', 'SN': '0000000000000033', 'SSLPort': 8443, 'Submask': '0x00FFFFFF', 'TCPMaxConn': 10, 'TCPPort': 34567, 'UDPPort': 34568, 'UseHSDownLoad': True, 'Version': 'V5.00.R02.000559A7.10010.040400.0020000', 'Brand': 'xm'}, '00:00:00:00:00:48': {'BuildDate': '2022- 2-23 16:34: 0', 'ChannelNum': 1, 'DeviceType': 0, 'GateWay': '0xFE01A8C0', 'HostIP': '0x0501A8C0', 'HostName': 'IVG-N4', 'HttpPort': 80, 'MAC': '00:00:00:00:00:48', 'MonMode': 'TCP', 'NetConnectState': 1, 'OtherFunction': 'D=2023-03-21 13:21:33 V=a721ad83d521bc7',
-               'SN': '000000000000002a', 'SSLPort': 8443, 'Submask': '0x00FFFFFF', 'TCPMaxConn': 10, 'TCPPort': 34567, 'UDPPort': 34568, 'UseHSDownLoad': False, 'Version': 'V5.00.R02.000629G3.10010.140200.0020000', 'Brand': 'xm'}, '00:00:00:00:00:6b': {'BuildDate': '2020-07-04 09:25:14', 'ChannelNum': 1, 'GateWay': '0x0101A8C0', 'HostIP': '0x0401A8C0', 'HostName': 'IVG-85HF20PYA-S', 'HttpPort': 80, 'MAC': '00:00:00:00:00:6b', 'MonMode': 'TCP', 'NetConnectState': 1, 'OtherFunction': 'D=2023-03-21 13:21:33 V=088a5643e0387f2', 'SN': '0000000000000095', 'SSLPort': 8443, 'Submask': '0x00FFFFFF', 'TCPMaxConn': 10, 'TCPPort': 34567, 'UDPPort': 34568, 'UseHSDownLoad': False, 'Version': 'V5.00.R02.000559A7.10010.040400.0020000', 'Brand': 'xm'}, '00:00:00:00:00:43': {'BuildDate': '2020-07-04 09:25:14', 'ChannelNum': 1, 'GateWay': '0x0201A8C0', 'HostIP': '0x0601A8C0', 'HostName': 'IVG-85HF20PYA-S', 'HttpPort': 80, 'MAC': '00:00:00:00:00:43', 'MonMode': 'TCP', 'NetConnectState': 1, 'OtherFunction': 'D=2023-03-21 13:21:34 V=6d1150f906f4948', 'SN': '0000000000000039', 'SSLPort': 8443, 'Submask': '0x00FFFFFF', 'TCPMaxConn': 10, 'TCPPort': 34567, 'UDPPort': 34568, 'UseHSDownLoad': False, 'Version': 'V5.00.R02.000559A7.10010.040400.0020000', 'Brand': 'xm'}, '00:00:00:00:00:22': {'BuildDate': '2020- 9- 1 14:38:48', 'ChannelNum': 1, 'GateWay': '0x0101A8C0', 'HostIP': '0x0901A8C0', 'HostName': 'camera_', 'HttpPort': 80, 'MAC': '00:00:00:00:00:22', 'MonMode': 'TCP', 'NetConnectState': 1, 'OtherFunction': 'D=2023-03-21 13:21:34 V=bdf396972028edf', 'SN': '0000000000000061', 'SSLPort': 8443, 'Submask': '0x00FFFFFF', 'TCPMaxConn': 10, 'TCPPort': 34567, 'UDPPort': 34568, 'UseHSDownLoad': True, 'Version': 'V5.00.R02.000529B2.10010.040600.0020000', 'Brand': 'xm'}})
+               'SN': '000000000000002a', 'SSLPort': 8443, 'Submask': '0x00FFFFFF', 'TCPMaxConn': 10, 'TCPPort': 34567, 'UDPPort': 34568, 'UseHSDownLoad': False, 'Version': 'V5.00.R02.000629G3.10010.140200.0020000', 'Brand': 'xm'}, '00:00:00:00:00:6b': {'BuildDate': '2020-07-04 09:25:14', 'ChannelNum': 1, 'GateWay': '0x0101A8C0', 'HostIP': '0x0401A8C0', 'HostName': 'IVG-85HF20PYA-S', 'HttpPort': 80, 'MAC': '00:00:00:00:00:6b', 'MonMode': 'TCP', 'NetConnectState': 1, 'OtherFunction': 'D=2023-03-21 13:21:33 V=088a5643e0387f2', 'SN': '0000000000000095', 'SSLPort': 8443, 'Submask': '0x00FFFFFF', 'TCPMaxConn': 10, 'TCPPort': 34567, 'UDPPort': 34568, 'UseHSDownLoad': False, 'Version': 'V5.00.R02.000559A7.10010.040400.0020000', 'Brand': 'xm'}, '00:00:00:00:00:43': {'BuildDate': '2020-07-04 09:25:14', 'ChannelNum': 1, 'GateWay': '0x0201A8C0', 'HostIP': '0x0601A8C0', 'HostName': 'IVG-85HF20PYA-S', 'HttpPort': 80, 'MAC': '00:00:00:00:00:43', 'MonMode': 'TCP', 'NetConnectState': 1, 'OtherFunction': 'D=2023-03-21 13:21:34 V=6d1150f906f4948', 'SN': '0000000000000039', 'SSLPort': 8443, 'Submask': '0x00FFFFFF', 'TCPMaxConn': 10, 'TCPPort': 34567, 'UDPPort': 34568, 'UseHSDownLoad': False, 'Version': 'V5.00.R02.000559A7.10010.040400.0020000', 'Brand': 'xm'}, '00:00:00:00:00:22': {'BuildDate': '2020- 9- 1 14:38:48', 'ChannelNum': 1, 'GateWay': '0x0101A8C0', 'HostIP': '0x6414A8C0', 'HostName': 'camera_', 'HttpPort': 80, 'MAC': '00:00:00:00:00:22', 'MonMode': 'TCP', 'NetConnectState': 1, 'OtherFunction': 'D=2023-03-21 13:21:34 V=bdf396972028edf', 'SN': '0000000000000061', 'SSLPort': 8443, 'Submask': '0x00FFFFFF', 'TCPMaxConn': 10, 'TCPPort': 34567, 'UDPPort': 34568, 'UseHSDownLoad': True, 'Version': 'V5.00.R02.000529B2.10010.040600.0020000', 'Brand': 'xm'}})
 else:
     debugLevel = logging.INFO
     devices = {}
 
-
 def log(*args):
     # logging.debug(*args)
     print(*args)
-
 
 logging.basicConfig(format='%(asctime)s> %(message)s',
                     level=debugLevel, datefmt='[%H:%M:%S]')
@@ -58,9 +59,10 @@ class MainWindow (QMainWindow):
     def filedialog(self):
         options = QFileDialog.Options()
         fileName, _ = QFileDialog.getOpenFileName(
-            self, "QFileDialog.getOpenFileName()", "", "All Files (*)", options=options)
+            self, "Select upgrade file", settings.value("fwdir", '.'), "Firmware files (*.bin)", options=options)
         if fileName:
             self.ui.labFilename.setText(fileName)
+            settings.setValue("fwdir", os.path.dirname(fileName))
 
     def getCheckedDevices(self, column):
         checked_list = []
@@ -97,15 +99,16 @@ class MainWindow (QMainWindow):
         # print(devices[1])
         self.ui.statusbar.showMessage(selected_dev)
 
-
 def onBtnSearch():
     global devices
+    QApplication.setOverrideCursor(Qt.WaitCursor)
     if not debug:
-        devices={}
+        devices = {}
         try:
             devices = udpSearch(devices)
         except Exception as error:
             print(" ".join([str(x) for x in list(error.args)]))
+    QApplication.restoreOverrideCursor()
     count = 0
     w.ui.tableWidget.setRowCount(len(devices))
     for dev in devices:
@@ -137,7 +140,7 @@ def str2ip(s):
 
 def udpSearch(devices):
     server = socket(AF_INET, SOCK_DGRAM)
-    server.bind(("", 34569))
+    server.bind((w.ui.comboIPAddresses.currentText(), 34569))
     server.settimeout(2)
     server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     server.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
@@ -160,16 +163,20 @@ def udpSearch(devices):
                 devices[answer["NetWork.NetCommon"]["MAC"]][u"Brand"] = u"xm"
     return devices
 
+
 def cbProgress(s):
     w.ui.statusbar.showMessage(s)
 
+
 def upgradeFirmware(ip, username, password, filename):
+    QApplication.setOverrideCursor(Qt.WaitCursor)
     cam = DVRIPCam(ip, username=username, password=password)
     if cam.login():
         log("Auth success")
         cam.upgrade(filename, 0x4000, cbProgress)
     else:
         log("Auth failed")
+    QApplication.restoreOverrideCursor
 
 
 def udpSetAddresses(data):
@@ -191,10 +198,9 @@ def udpSetAddresses(data):
     config = json.dumps(
         config, ensure_ascii=False, sort_keys=True, separators=(", ", " : ")
     ).encode("utf8")
-    log(config)
     if not debug:
         server = socket(AF_INET, SOCK_DGRAM)
-        server.bind(("", 34569))
+        server.bind((w.ui.comboIPAddresses.currentText(), 34569))
         server.settimeout(1)
         server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         server.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
@@ -238,37 +244,77 @@ def udpSetAddresses(data):
 
 def btnSave():
     if w.ui.tableWidget.selectedItems():
+        QApplication.setOverrideCursor(Qt.WaitCursor)
         selected_dev = w.ui.tableWidget.item(
             w.ui.tableWidget.currentRow(), 3).text()
         udpSetAddresses(['config', selected_dev, w.ui.editIPAddress.text(
         ), w.ui.editSubnetAddress.text(), w.ui.editGatewayAddress.text(), ''])
+    QApplication.restoreOverrideCursor()
 
 
 def btnUpgrade():
     if w.ui.tableWidget.selectedItems() and w.ui.labFilename.text():
+        QApplication.setOverrideCursor(Qt.WaitCursor)
         selected_dev = w.ui.tableWidget.item(
             w.ui.tableWidget.currentRow(), 1).text()
         upgradeFirmware(w.ui.editIPAddress.text(), w.ui.editUsername.text(
         ), w.ui.editCurrentPassword.text(), w.ui.labFilename.text())
+    QApplication.restoreOverrideCursor()
 
+def netipLogin():
+    global cam
+    if w.ui.tableWidget.selectedItems():
+        selected_dev = w.ui.tableWidget.item(
+            w.ui.tableWidget.currentRow(), 1).text()
+        ip=w.ui.editIPAddress.text()
+        username=w.ui.editUsername.text()
+        password=w.ui.editCurrentPassword.text()
+
+        QApplication.setOverrideCursor(Qt.WaitCursor)
+        if not cam.socket:
+            cam = DVRIPCam(ip, username=username, password=password)
+            cam.logger(None)
+            if cam.login():
+                w.ui.statusbar.showMessage(w.tr("Connected"))
+            else:
+                w.ui.statusbar.showMessage(w.tr("Login failed"))
+            QApplication.restoreOverrideCursor()
+            return cam
+        
+
+def netipSendCommand(command, *args):
+    if cam.login():
+        getattr(cam,command)(*args)
+        w.ui.statusbar.showMessage(w.tr(f"{command} OK"))
+    else:
+        w.ui.statusbar.showMessage(w.tr("Command failed"))
+
+def getDigitalChannels():
+    if cam.login():
+        info = cam.get_info("NetWork.RemoteDeviceV3")
+        print(json.dumps(info, ensure_ascii=False))
 
 def onComboSelectInterface():
     iface = str(w.ui.comboInterfaces.currentText())
     w.ui.comboIPAddresses.clear()
-    w.ui.comboIPAddresses.addItem(
-        str(netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr']))
+    for addr_info in netifaces.ifaddresses(iface)[netifaces.AF_INET]:
+        w.ui.comboIPAddresses.addItem(
+            str(addr_info['addr']))
     w.ui.comboIPAddresses.setCurrentText(
         str(netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr']))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
+    app.setOrganizationName("OpenIPC")
+    app.setOrganizationDomain("openipc.org")
+    app.setApplicationName("DMS")
+    settings = QSettings()
     # locale = getdefaultlocale()
-    locale = 'ru_RU'
+    locale = 'ru'
 
     translator = QtCore.QTranslator(app)
-    translator.load('res/qt_%s.qm' % locale[0])
+    translator.load('res/%s.qm' % locale[0])
     app.installTranslator(translator)
 
     w = MainWindow()
@@ -279,12 +325,15 @@ if __name__ == '__main__':
     w.ui.btnBrowse.clicked.connect(w.filedialog)
     w.ui.btnSave.clicked.connect(btnSave)
     w.ui.btnUpgrade.clicked.connect(btnUpgrade)
+    w.ui.btnReboot.clicked.connect(lambda: netipSendCommand("reboot"))
+    w.ui.btnDigitalChannels.clicked.connect(getDigitalChannels)
     w.ui.btnChangePassword.clicked.connect(w.btnChangePassword)
     w.ui.tableWidget.cellClicked.connect(w.onRowClick)
+    w.ui.tableWidget.cellDoubleClicked.connect(netipLogin)
     w.ui.comboInterfaces.currentTextChanged.connect(onComboSelectInterface)
 
-    w.ui.tableWidget.setHorizontalHeaderLabels(
-        ["#", "IP", "Port", "MAC", "CloudID", "Version", "Date", "Name"])
+    # w.ui.tableWidget.setHorizontalHeaderLabels(
+    #     ["#", "IP", "Port", "MAC", "CloudID", "Version", "Date", "Name"])
     w.ui.tableWidget.horizontalHeader().resizeSection(0, 1)
     w.ui.tableWidget.horizontalHeader().setSectionResizeMode(
         QHeaderView.ResizeToContents)
@@ -294,10 +343,12 @@ if __name__ == '__main__':
     hostdefaultgw = netifaces.gateways()['default'][netifaces.AF_INET][1]
     badinterfaces = ['lo', 'docker', 'veth', 'vbox']
     for iface in hostinterfaces:
-        if netifaces.AF_INET not in netifaces.ifaddresses(iface):
+        addrs = netifaces.ifaddresses(iface)
+        if netifaces.AF_INET not in addrs:
             badinterfaces.append(iface)
     w.ui.comboInterfaces.addItems(
         [x for x in hostinterfaces if not any(x.startswith(s) for s in badinterfaces)])
-    w.ui.comboInterfaces.setCurrentText(hostdefaultgw)
+    if hostdefaultgw:
+        w.ui.comboInterfaces.setCurrentText(hostdefaultgw)
 
     sys.exit(app.exec_())
